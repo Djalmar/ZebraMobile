fbdb8fc0b84748defa7c90d5e1138401433328c2
D'jalmar Gutierrez Titirico <dajaman1@hotmail.com>
2013-04-25 09:35:52 -0400

    Mi primer commit

---
+++ Zebra.WPApp/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Zebra.WPApp.Resources;
+
+namespace Zebra.WPApp
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
