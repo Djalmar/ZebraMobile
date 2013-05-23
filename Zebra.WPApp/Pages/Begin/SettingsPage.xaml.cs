2070576f67d9f3af85a320f5c35720b33e8e3d4e
D'jalmar Gutierrez Titirico <dajaman1@hotmail.com>
2013-05-23 11:05:31 -0400

    DONE. app abierta por primera vez

--- Zebra.WPApp/Pages/Begin/SettingsPage.xaml.cs
+++ Zebra.WPApp/Pages/Begin/SettingsPage.xaml.cs
@@ -9,15 +9,23 @@ namespace Zebra.WPApp.Pages.Begin
         public SettingsPage()
         {
             InitializeComponent();
+            this.Loaded += SettingsPage_Loaded;
         }
 
+        private void SettingsPage_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            
+        }
         private void brdNextButton_Tap_1(object sender, System.Windows.Input.GestureEventArgs e)
         {
             //podriamos considerar preguntarle al usuario si esta seguro de continuar, ademas avisarle q podra cambiar esto en
             //el menu de configuraciones
+
             NavigationService.Navigate(new Uri("/Pages/Begin/MenuPage.xaml", UriKind.RelativeOrAbsolute));
             //aqui recien decimos q el usuario entro por primera vez a la app
             App.FirstTimeLaunch = true;
+
+            //tenemos que guardar la configuracion que el usuario a ingresado -> IsoaltedStorge
         }
     }
 }
--- Zebra.WPApp/Pages/Begin/SplashPage.xaml.cs
+++ Zebra.WPApp/Pages/Begin/SplashPage.xaml.cs
@@ -12,29 +12,50 @@ namespace Zebra.WPApp.Pages.Begin
             InitializeComponent();
             stbTxbTitle.Completed += SplashPage_Completed;
             mdeVideo.MediaEnded += mdeVideo_MediaEnded;
+            btnContinue.Tap += btnContinue_Tap;
+            btnReplay.Tap += btnReplay_Tap;
             this.Loaded += SplashPage_Loaded;
         }
 
-        private void SplashPage_Loaded(object sender, RoutedEventArgs e)
+        void SplashPage_Loaded(object sender, RoutedEventArgs e)
         {
             stbTxbTitle.Begin();
         }
 
-        private void SplashPage_Completed(object sender, EventArgs e)
+        void SplashPage_Completed(object sender, EventArgs e)
         {
+            txbTitle.Visibility = System.Windows.Visibility.Collapsed;
             mdeVideo.Play();
         }
 
-        private void mdeVideo_MediaEnded(object sender, RoutedEventArgs e)
+        void mdeVideo_MediaEnded(object sender, RoutedEventArgs e)
         {
             stkButtons.Visibility = System.Windows.Visibility.Visible;
             stbStkButtons.Begin();
             stbStkButtons.Completed += stbStkButtons_Completed;
         }
 
-        private void stbStkButtons_Completed(object sender, EventArgs e)
+        void stbStkButtons_Completed(object sender, EventArgs e)
         {
-            NavigationService.Navigate(new Uri("/Pages/Begin/SettingsPage.xaml", UriKind.RelativeOrAbsolute));
+            //a ver si le metemos alguna animacion
         }
+
+        void btnReplay_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            mdeVideo.Play();
+            //podriamos revertir la animacion
+            stkButtons.Visibility = System.Windows.Visibility.Collapsed;
+        }
+
+        void btnContinue_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            //Preguntar al usuario si esta seguro de continuar
+            var result = MessageBox.Show("Are you sure ??", "Continue", new MessageBoxButton());
+            if (MessageBoxResult.OK == result)
+            {
+                NavigationService.Navigate(new Uri("/Pages/Begin/SettingsPage.xaml", UriKind.RelativeOrAbsolute));
+            }
+        }
+
     }
 }
