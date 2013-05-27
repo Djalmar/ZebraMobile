b8e0317421e98df618047173ec2d409b7cc17b45
D'jalmar Gutierrez Titirico <dajaman1@hotmail.com>
2013-05-26 21:55:09 -0400

    Push Pin
    Sugestions ?? I'm using an user control it's ok ??

---
+++ Zebra.WPApp/UserControls/uscPushPin.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Zebra.WPApp.UserControls
+{
+    public partial class uscPushPin : UserControl
+    {
+        public uscPushPin()
+        {
+            InitializeComponent();
+        }
+        //aqui llenamos el push pin
+        public uscPushPin(string category, SolidColorBrush color,Uri direccion)
+        {
+            InitializeComponent();
+            txbCategory.Text = category;
+            elpPush.Fill = color;
+            imgIconCategory.Source = new BitmapImage(direccion);
+        }
+    }
+}
