cf4b26ca32d17af1d62a8aa50597a59ec8625977
D'jalmar Gutierrez Titirico <dajaman1@hotmail.com>
2013-06-12 22:04:26 -0400

    SelectedPlace parcialChanges
    animaciones en el listBox de servicios, rating direccion precios

--- Zebra.WPApp/Pages/Places/SelectedPlacePage.xaml.cs
+++ Zebra.WPApp/Pages/Places/SelectedPlacePage.xaml.cs
@@ -21,9 +21,30 @@ namespace Zebra.WPApp.Pages.Places
 
         void SelectedPlacePage_Loaded(object sender, RoutedEventArgs e)
         {
+            staticClasses.selectedPlace.rating /= 2;
             panItemName.DataContext = staticClasses.selectedPlace;
+            List<Service> listaServicios=CrearListadeServicios();
+            //lstPlaceFeatures.ItemsSource = listaServicios;
+            expFeatures.ItemsSource = listaServicios;
         }
 
+        private List<Service> CrearListadeServicios()
+        {
+            return new List<Service>()
+            {
+                new Service(){Name="delivery",Exist=staticClasses.selectedPlace.delivery},
+                new Service(){Name="holidays",Exist=staticClasses.selectedPlace.holidays},
+                new Service(){Name="kids area",Exist=staticClasses.selectedPlace.kidsArea},
+                new Service(){Name="delivery",Exist=staticClasses.selectedPlace.delivery}
+            };
+        }
+
+        
+        public class Service
+        {
+            public string Name { get; set; }
+            public bool Exist { get; set; }
+        }
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedFrom(e);
